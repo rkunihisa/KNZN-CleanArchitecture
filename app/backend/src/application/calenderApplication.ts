d0947fea3c01bce0a9cb9c59f75c1b3e625c3075
Ryota Kunihisa <r.kunihisa@infocom.co.jp>
2025-07-07 12:04:19 +0900

    add: involke application at index.ts

--- app/backend/src/application/calenderApplication.ts
+++ app/backend/src/application/calenderApplication.ts
@@ -2,13 +2,13 @@ import { GoogleCalenderService } from "../domain/service/googleCalenderService";
 import { GoogleCalenderClient } from "../infrastructure/googleCalenderClient";
 
 export class CalenderApplication {
-  create() {
+  async create() {
     // GoogleCalendarClientを実体化する。こいつの見た目はGoogleCalendarInterface
     const googleCalenderClient = new GoogleCalenderClient();
     // GoogleCalenderServiceはGoogleCalenderInterfaceを受け取るので、GoogleCalenderClientを渡せる
     const googleCalenderService = new GoogleCalenderService(googleCalenderClient);
 
-    const eventList = googleCalenderService.createEventList()
+    const eventList = await googleCalenderService.createEventList()
 
     return eventList;
   }
