4ba0fd4980aeda304183a94c0630e4828cad5fc1
Ryota Kunihisa <r.kunihisa@infocom.co.jp>
2025-07-11 08:26:00 +0900

    update: test sattings

--- app/backend/jest.config.js
+++ app/backend/jest.config.js
@@ -3,10 +3,22 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/test", "<rootDir>/src"],
+  transform: {
+    // @swc/jestを使用してテストを高速に実行する
+    '^.+\\.tsx?$': ['@swc/jest', {}],
+  },
+   moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+  },
+  testPathIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "<rootDir>/dist/",
+    "<rootDir>/src/index.ts"],
   collectCoverage: true,
   collectCoverageFrom: [
     "**/*.ts",
     "!**/node_modules/**",
+    "!**/index.ts",
   ],
   // coverageDirectory: 'coverage_dir',
 };
