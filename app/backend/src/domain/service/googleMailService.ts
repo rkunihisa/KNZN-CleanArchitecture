8ceb2f502a35b7569c2b32ddc9daa73f33a16002
Ryota Kunihisa <116238223+rkunihisa@users.noreply.github.com>
2025-07-15 08:33:10 +0000

    add: implement getMailTitles method and update test plan for GoogleMailService

--- app/backend/src/domain/service/googleMailService.ts
+++ app/backend/src/domain/service/googleMailService.ts
@@ -1,5 +1,24 @@
 // add service here
 // 要件
-// - メソッド1. 取得したメールタイトルの一覧を格納したlistを返す
-// - メソッド2. メールの本文の最初の10文字を格納したlistを返す
-// - メソッド3. 一番新しいメールのタイトルを返す
+
+import { GoogleMailInterface } from "../model/googleMailInterface";
+
+export class GoogleMailService {
+    private googleMail: GoogleMailInterface;
+    
+    constructor(googleMail: GoogleMailInterface) {
+        this.googleMail = googleMail;
+    }
+	
+  // - メソッド1. 取得したメールタイトルの一覧を格納したlistを返す
+  async getMailTitles(): Promise<string[]> {
+    const fetchedMails = await this.googleMail.fetchMails();
+    const fetchedMailTitles = fetchedMails.map((value) => value["id"])
+		console.log("fetchedMailTitles:", fetchedMailTitles)
+    return fetchedMailTitles
+  }    
+
+  // - メソッド2. メールの本文の最初の10文字を格納したlistを返す
+  // - メソッド3. 一番新しいメールのタイトルを返す
+
+} 
